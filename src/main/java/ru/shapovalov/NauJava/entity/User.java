c0d180ce6958ffa0d53175959e0ebfc4b3c190c9
AlexandrSh12 <alexandr.shapovalov12@gmail.com>
2026-04-19 22:13:51 +0700

    task 6 impl

--- src/main/java/ru/shapovalov/NauJava/entity/User.java
+++ src/main/java/ru/shapovalov/NauJava/entity/User.java
@@ -1,4 +1,5 @@
 package ru.shapovalov.NauJava.entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,16 +14,23 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String username;
+    private String password;
     private String email;
     private String phone;
 
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    @JsonIgnore
     @OneToMany(mappedBy = "user")
     private List<Item> items;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "user")
     private List<Favorite> favorites;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "user")
     private List<Comment> comments;
 }
